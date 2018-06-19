26f8f0d638a65cca00e56d2a6a9e9e3bb5d8ca59
m1k1e <mikle.fedorenko@gmail.com>
2018-06-19 15:58:44 +0300

    Complete CSVReader application

--- src/readcsvapp/CSVReader.java
+++ src/readcsvapp/CSVReader.java
@@ -1,9 +1,42 @@
 package readcsvapp;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVReader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// Work 
+		String csvFile = "/home/user/Документи/Java/WorkingWithCSVFiles/src/readcsvapp/country.csv";
+		BufferedReader br = null;
+		String line = "";
+		String csvSplitBy = ",";
+		
+		try {
+			// Open csv file
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				//use comma separator
+				String[] country = line.split(csvSplitBy);
+				
+				System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+			}
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
 
 	}
 
